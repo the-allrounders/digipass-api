d8359581e9813bc1ddfae538263ede603e2c6de7
Bart Langelaan <langelaanbart@gmail.com>
2016-06-05 15:09:10 +0200

    Removed joi and express-validation

--- server/routes/category.js
+++ server/routes/category.js
@@ -1,7 +1,5 @@
 import express from 'express';
-import validate from 'express-validation';
 import categoryCtrl from '../controllers/category';
-import paramValidation from '../../config/param-validation';
 
 const router = express.Router();	// eslint-disable-line new-cap
 
@@ -10,14 +8,14 @@ router.route('/')
     .get(categoryCtrl.list)
 
     /** POST /api/categories - Create new category */
-    .post(validate(paramValidation.createCategory), categoryCtrl.create);
+    .post(categoryCtrl.create);
 
 router.route('/:categoryId')
 /** GET /api/categories/:categoryId - Get category */
     .get(categoryCtrl.get)
 
     /** PUT /api/categories/:categoryId - Update category */
-    .put(validate(paramValidation.updateCategory), categoryCtrl.update)
+    .put(categoryCtrl.update)
 
     /** DELETE /api/categories/:categoryId - Delete category */
     .delete(categoryCtrl.remove);

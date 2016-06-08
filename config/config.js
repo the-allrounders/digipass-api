8880048ce6b789dab7477b67fb66d59c8a0efebf
Bart Langelaan <langelaanbart@gmail.com>
2016-06-08 02:01:58 +0200

    Add extra environment variable

--- config/config.js
+++ config/config.js
@@ -2,5 +2,5 @@
 
 module.exports = {
     port: process.env.PORT || 3000,
-    mongo: process.env.MONGODB_URI || "mongodb://localhost"
+    mongo: process.env.MONGODB_URI || "mongodb://" + (process.env.MONGO_PORT_27017_TCP_ADDR || 'localhost')
 };

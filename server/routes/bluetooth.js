caae7c1eac48588adec9d219a88cc334f319f6ce
Bart Langelaan <langelaanbart@gmail.com>
2016-06-21 17:02:29 +0200

    Make POST /bluetooth work

--- server/routes/bluetooth.js
+++ server/routes/bluetooth.js
@@ -1,12 +1,42 @@
 'use strict';
 
 const router = require('express').Router(),
-    Organisations = require('../models/organisation');
+    Organisations = require('../models/organisation'),
+    passport = require('passport'),
+    Permission = require('../models/permission');
 
-router.route('/').post((req, res) => {
-    Organisations.find({'devices.bluetooth': req.body.bluetooth}).then((results) => {
-        res.json(results);
-    });
+router.route('/').post(passport.authenticate('bearer', {session: false}), (req, res) => {
+    Organisations
+        .find({devices: {$elemMatch: {bluetooth: req.body.bluetooth}}})
+        .then(organisations => {
+
+            // Remove the token from all the results, it's secret!
+            organisations.forEach(result => result.token = null);
+
+            // Return the results
+            res.json(organisations);
+
+            // Find all preferences
+            organisations.forEach(organisation => organisation.devices[0].preferences.forEach(preference =>
+
+                // Search for permission
+                Permission.findOne({preference: preference}).then(permission => {
+
+                    if (!permission) {
+                        // If the permission does not exist, create it
+                        permission = new Permission({
+                            preference: preference,
+                            user: req.user._id,
+                            organisation: organisation._id,
+                            status: 'pending'
+                        });
+                    }
+                    permission.save();
+                    
+                })
+            ));
+
+        });
 });
 
 module.exports = router;
